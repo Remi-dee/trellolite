69bd271aa744bb29e9634f66d22beaddc1516daf
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-12 08:09:17 +0000

    Jul 12, 2023, 9:09 AM

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -36,7 +36,7 @@ function Modal() {
               leaveTo="opacity-0"
             >
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all ">
-                <Dialog.Title as = "h3" className = "text-lg font-medium leading -6 text-gray-500 p" >
+                <Dialog.Title as = "h3" className = "text-lg font-medium leading -6 text-gray-500" >
 
 
                 </Dialog.Title>
