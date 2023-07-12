f81f1c368919d563ed61969231d23c5d3e0966f4
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-12 08:28:41 +0000

    Jul 12, 2023, 9:28 AM

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -35,10 +35,10 @@ function Modal() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all ">
+              <Dialog.Panel className="flex w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all items-center justify-center ">
                 <Dialog.Title
                   as="h3"
-                  className="text-lg font-medium leading-6 text-gray-500 pb-2"
+                  className="text-lg font-medium leading-6 text-gray-500 pb-2 "
                 >
                   Add a Task
                 </Dialog.Title>
