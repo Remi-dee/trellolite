59bbf989348f788fe507c8dbc9a85bedb7c5206c
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-12 08:33:25 +0000

    Jul 12, 2023, 9:33 AM

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -25,7 +25,7 @@ function Modal() {
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
-          <div className="fixed min-h-full items-center justify-center p-4 text-center">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
