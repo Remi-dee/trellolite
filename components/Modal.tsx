93f4a95b4137ca04b5150396b368ca4cdba46b47
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-13 12:34:19 +0000

    Jul 13, 2023, 1:34 PM

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,6 +3,7 @@ import { useState, Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useModalStore } from "@/store/ModalStore";
 import { useBoardStore } from "@/store/BoardStore";
+import TaskTypeRadioGroup from "./TaskTypeRadioGroup";
 
 function Modal() {
   const [newTaskInput, setNewTaskInput] = useBoardStore((state) => [
