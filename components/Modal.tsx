1700cac8c36d614967d39fd9e9658f06650d5aa2
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-14 17:58:09 +0100

    added

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,34 +9,40 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 
 function Modal() {
   const imagePickerRef = useRef<HTMLInputElement>(null);
-  const [addTask, setImage, image, newTaskInput, setNewTaskInput] = useBoardStore(
-    (state) => [
-     state.addTask,
+  const [addTask, newTaskType, setImage, image, newTaskInput, setNewTaskInput] =
+    useBoardStore((state) => [
+      state.newTaskType,
+      state.addTask,
       state.image,
       state.setImage,
       state.newTaskInput,
 
       state.setNewTaskInput,
-    ]
-  );
+    ]);
   const [isOpen, closeModal] = useModalStore((state) => [
     state.isOpen,
     state.closeModal,
   ]);
 
-const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-  e.preventDefaut()
-  if(!newTaskInput) return
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefaut();
+    if (!newTaskInput) return;
 
-  setImage(null)
-  closeModal()
+    addTask(newTaskInput, newTaskType, image);
 
-}
+    setImage(null);
+    closeModal();
+  };
 
   return (
     // Use the `Transition` component at the root level
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="form" onSubmit={handleSubmit} className="relative z-10" onClose={closeModal}>
+      <Dialog
+        as="form"
+        onSubmit={handleSubmit}
+        className="relative z-10"
+        onClose={closeModal}
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -115,11 +121,15 @@ const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
                 </div>
 
                 <div className="mt-4">
-                  <button type = "submit"
-                  disabled = {!newTaskInput}
-                  className="inline-flex justify-center rounded border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2
+                  <button
+                    type="submit"
+                    disabled={!newTaskInput}
+                    className="inline-flex justify-center rounded border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2
                   focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-100 disabled:text-gray-300 disabled:cursor-not-allowed"
-                  > Add Task</button>
+                  >
+                    {" "}
+                    Add Task
+                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
