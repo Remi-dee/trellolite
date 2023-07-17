6735fa3304d9f00be435cf2ba8b0a68dc3ba6e3c
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-17 18:08:45 +0000

    Jul 17, 2023, 7:08 PM

--- components/Column.tsx
+++ components/Column.tsx
@@ -94,7 +94,6 @@ const Column = ({ id, todos, index }: Props) => {
                     className="w-full text-green-500 hover:text-green-600"
                   >
                     <div className="flex justify-between p-2 ">
-                    
                       <PlusCircleIcon className="w-10" />
                       <h2>Add a card</h2>
                     </div>
