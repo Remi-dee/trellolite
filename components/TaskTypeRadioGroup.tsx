f06825fd3dd3cb975bd10d1449fbda4ae969435a
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-17 21:13:24 +0000

    Jul 17, 2023, 10:13 PM

--- components/TaskTypeRadioGroup.tsx
+++ components/TaskTypeRadioGroup.tsx
@@ -7,7 +7,7 @@ import { CheckCircleIcon } from "@heroicons/react/24/solid";
 const types = [
   {
     id: "todo",
-    name: "Todo",
+    name: "Backlog",
     description: "A newtask to be completed",
     color: "bg-red-500",
   },
