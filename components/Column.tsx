9f559fcb347629dc779836fb1230a163bfca217e
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-17 18:03:57 +0000

    Jul 17, 2023, 7:03 PM

--- components/Column.tsx
+++ components/Column.tsx
@@ -93,7 +93,7 @@ const Column = ({ id, todos, index }: Props) => {
                     onClick={handleAddTodo}
                     className="text-green-500 hover:text-green-600"
                   >
-                    <div className="flex p-2 ">
+                    <div className=" p-2 ">
                     
                       <PlusCircleIcon />
                       <span>Add a card</span>
