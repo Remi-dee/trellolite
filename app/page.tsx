9e6fe2f5de5b8e68e806055ecf23d2d1d3785942
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-17 19:33:12 +0000

    Jul 17, 2023, 8:33 PM

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-[url('/')]">
+    <main className="bg-[url('public/')]">
       {/*Header */}
 
       <HeaderTop />
