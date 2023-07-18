3b21692508fa6d8c1e550648783d43502746b648
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-18 11:25:29 +0000

    Jul 18, 2023, 12:25 PM

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <HeaderTop />
     
         <Board /> 
-        <h1 className="absolute bottom-3 right-0 mx-11 text-white text-sm">Trellolite Ai, brought to you by &copy; Remi Daniel</h1>
+        <h1 className="absolute bottom-3 right-0 mx-11 text-white ">Trellolite Ai, brought to you by &copy; Remi Daniel</h1>
      
     </main>
   );
